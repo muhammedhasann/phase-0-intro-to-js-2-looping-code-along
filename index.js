ecba247246433e060cfbed6ac32b5201b37e21a0
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-03-20 21:25:13 +0300

    d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+/* 
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return writeCards;
+} */
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+    let i = 0;
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
+}
